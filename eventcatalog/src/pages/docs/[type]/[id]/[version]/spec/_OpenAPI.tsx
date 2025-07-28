598d4d223266320e402a2ef38a806a147d123224
Marcin Sodkiewicz <sodkiewicz.m@gmail.com>
2025-07-28 10:18:09 +0200

    issue-1631: Revert removal of ClientRouter

--- eventcatalog/src/pages/docs/[type]/[id]/[version]/spec/_OpenAPI.tsx
+++ eventcatalog/src/pages/docs/[type]/[id]/[version]/spec/_OpenAPI.tsx
@@ -1,16 +1,26 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ApiReferenceReact } from '@scalar/api-reference-react';
 import '@scalar/api-reference-react/style.css';
 import './_styles.css';
-const OpenAPISpec = ({ spec }: { spec: string }) => {
+const OpenAPISpec = ({ spec, url }: { spec?: string, url: string }) => {
   const [loaded, setLoaded] = useState(false);
+  const [openApiSpec, setOpenApiSpec] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(url)
+      const data = await result.json();
+      setOpenApiSpec(data);
+    }
+    fetchData();
+  }, [url]);
+
   return (
     <div>
       {!loaded && <div>Loading...</div>}
       <ApiReferenceReact
         configuration={{
           spec: {
-            content: spec,
+            content: spec || openApiSpec,
           },
           theme: 'fastify',
           hideClientButton: true,
